5e036b9e437ee840d016cd028313d3352d872cd8
Jason.Zhou <jason.zhou@edoctor.cn>
2017-11-13 15:14:38 +0800

    1

---
+++ 自定义TabBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  自定义TabBar
+//
+//  Created by Yang on 2017/11/13.
+//  Copyright © 2017年 A589. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 自定义TabBar/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  自定义TabBar
+//
+//  Created by Yang on 2017/11/13.
+//  Copyright © 2017年 A589. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
